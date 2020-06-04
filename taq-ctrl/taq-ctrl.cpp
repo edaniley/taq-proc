ae5a97b53ef30c839cca8fafbea89c15c67bc640
edaniley <edaniley@gmail.com>
2020-06-04 18:52:44 +0100

    taq-ctrl improvement

--- taq-ctrl/taq-ctrl.cpp
+++ taq-ctrl/taq-ctrl.cpp
@@ -22,6 +22,7 @@ namespace fs = boost::filesystem;
 namespace mm = boost::interprocess;
 
 string file_path;
+bool pretty, sorted, no_header;
 
 struct separate_thousands : numpunct<char> {
   char_type do_thousands_sep() const override { return ','; }
@@ -33,7 +34,7 @@ string ToString(int value, int width) {
   ss.imbue(locale(""));
   ss << value;
   string str = ss.str();
-  str.insert(str.begin(), 10 - str.size(), ' ');
+  str.insert(str.begin(), width - str.size(), ' ');
   return str;
 }
 
@@ -45,19 +46,32 @@ void HandleNbboFile(const FileHeader& fh, const mm::mapped_region & mm_region) {
   if ((sizeof(fh) + fh.symb_cnt * sizeof(SymbolMap) + fh.rec_cnt  * sizeof(Nbbo))  != mm_region.get_size()) {
     throw domain_error("Input file corruption : " + file_path);
   }
-  auto thousands = make_unique<separate_thousands>();
-  cout.imbue(locale(cout.getloc(), thousands.release()));
-  cout << "date file     " << file_path << endl;
-  cout << "file size     " << mm_region.get_size() << endl;
-  cout << "record type   " << "Nbbo" << endl;
-  cout << "symbol count  " << fh.symb_cnt << endl;
-  cout << "symbols" << endl;
-  const SymbolMap* symbols =  (const SymbolMap * )((char *)(mm_region.get_address()) + sizeof(fh) + fh.rec_cnt * sizeof(Nbbo));
+  if (false == no_header) {
+    auto thousands = make_unique<separate_thousands>();
+    auto saved_locale = cout.imbue(locale(cout.getloc(), thousands.release()));
+    cout << "date file     " << file_path << endl;
+    cout << "file size     " << mm_region.get_size() << endl;
+    cout << "record type   " << "Nbbo" << endl;
+    cout << "symbol count  " << fh.symb_cnt << endl;
+    cout << "symbols" << endl;
+    cout.imbue(saved_locale);
+  }
+  const SymbolMap* symbol_map =  (const SymbolMap * )((char *)(mm_region.get_address()) + sizeof(fh) + fh.rec_cnt * sizeof(Nbbo));
+  vector<pair<string, int>> symbols;
   for (int i = 0; i < fh.symb_cnt; i++) {
-    const SymbolMap& map = symbols[i];
-    string symbol = map.symb;
-    symbol.append(12 - symbol.length(), ' ');
-    cout << symbol << ToString(map.end - map.start + 1, 10) << endl;
+    const SymbolMap& map = symbol_map[i];
+    symbols.push_back(make_pair(map.symb, map.end - map.start + 1));
+  }
+  if (sorted) {
+    sort(symbols.begin(), symbols.end(), [] (const auto & left, const auto& right) {return right.second < left.second;});
+  }
+  for (auto & symbol : symbols ) {
+    if (pretty) {
+      symbol.first.append(12 - symbol.first.length(), ' ');
+      cout << symbol.first  << ToString(symbol.second, 10) << endl;
+    } else {
+      cout << symbol.first<< ","  << symbol.second << endl;
+    }
   }
 }
 
@@ -67,11 +81,21 @@ int main(int argc, char** argv) {
   desc.add_options()
     ("help,h", "produce help message")
     ("file,f", po::value<string>(&file_path), "path to datafile")
+    ("pretty", po::bool_switch(&pretty)->default_value(false), "use pretty formatting")
+    ("sort", po::bool_switch(&sorted)->default_value(false), "sort symbols by record count in descending order")
+    ("no-header", po::bool_switch(&no_header)->default_value(false), "print output without summary header")
     ;
   po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
+  try {
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+  } catch (exception & ex) {
+    cerr << ex.what() << endl << endl;
+    cerr << desc << endl;
+    return 2;
+  }
   if (vm.count("help")) {
+    cout << desc << endl;
     return 1;
   }
   try {
