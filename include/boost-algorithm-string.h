a4a00d3ad13023fcd9a2f6b4036be10300ff8ae0
Edward Danileyko <edaniley@localhost.localdomain>
2020-06-15 22:04:35 -0400

    added tick-calc/tick-net.cpp skeleton for Linux all builds are ok

--- include/boost-algorithm-string.h
+++ include/boost-algorithm-string.h
@@ -2,14 +2,14 @@
 #ifndef BOOST_ALGORITHM_STRING_INCLUDED
 #define BOOST_ALGORITHM_STRING_INCLUDED
 
-#ifndef GCC_VERSION
+#ifdef __unix__
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 #endif
 
 #include <boost/algorithm/string.hpp>
 
-#ifndef GCC_VERSION
+#ifdef __unix__
 #pragma GCC diagnostic pop
 #endif
 
---
+++ tick-calc/tick-net.cpp
@@ -0,0 +1,24 @@
+#include <exception>
+#include <iostream>
+#include "tick-calc.h"
+#include "tick-conn.h"
+
+using namespace std;
+using namespace Taq;
+
+namespace tick_calc {
+
+#ifdef __unix__
+
+
+void NetInitialize(AppAruments & ) {
+
+}
+
+
+void NetPoll(AppAruments&) {
+}
+
+#endif
+
+}
