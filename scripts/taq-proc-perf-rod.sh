4993ea54f2f7443542b420b7f2699c642c607df5
edaniley <edaniley@gmail.com>
2020-11-01 20:06:55 +0000

    add performance testing scripts for ROD

---
+++ scripts/taq-proc-perf-rod.sh
@@ -0,0 +1,23 @@
+echo "job:single runs:10 parallel:1 file:sample.csv markouts:n/a function:ROD"
+for i in {1..10}; do
+  python3 test-rod.py -t 35.239.195.67:3090 -i sample.csv -s 4173312
+done
+
+cmdfile=$(mktemp)
+echo "job:single runs:10 parallel:4 file:sample.csv markouts:n/a function:ROD"
+for i in {1..10}; do
+  echo "-t35.239.195.67:3090 -isample.csv -s4173312" >> $cmdfile
+done
+for i in {1..10}; do
+  cat $cmdfile | parallel -j4 --colsep ' ' python3 test-rod.py {1} {2} {3}
+done
+
+cp /dev/null $(mktemp)
+echo "job:single runs:10 parallel:10 file:sample.csv markouts:n/a function:ROD"
+for i in {1..10}; do
+  echo "-t35.239.195.67:3090 -isample.csv -s4173312" >> $cmdfile
+done
+for i in {1..10}; do
+  cat $cmdfile | parallel -j4 --colsep ' ' python3 test-rod.py {1} {2} {3}
+done
+rm $cmdfile
--- scripts/test-rod.py
+++ scripts/test-rod.py
@@ -23,8 +23,22 @@ in_cid = 14
 in_date = 15
 in_mpid = 16
 
+line_no = 0
+
+def StringToDuration(val):
+  tmp = val.split('.')
+  if len(tmp) == 1:
+    val += ".000000"
+  elif len(tmp[1]) > 6:
+    to_trim = len(tmp[1]) - 6
+    val = val[:len(val)-(len(tmp[1]) - 6)]
+  elif len(tmp[1]) < 6:
+    val = tmp[0] + '.' + '{:<06s}'.format(tmp[1])
+  dt = datetime.strptime(val, "%H:%M:%S.%f")
+  return timedelta(hours=dt.hour, minutes=dt.minute, seconds=dt.second, microseconds=dt.microsecond)
 
 def TestROD(args):
+  global line_no
   argument_mapping = []
   argument_mapping.append({
     "function" : "ROD",
@@ -59,15 +73,16 @@ def TestROD(args):
 
   file = open(args.in_file)
   line = file.readline()
+  line_no = 1
   while line:
     rec = line.rstrip("\n").split(",")
     if len(rec[0])>0:
       OrdID = "{}.{}".format(rec[in_mpid], rec[in_order_id])
       Symbol = rec[in_symbol]
       Date = rec[in_date]
-      StartTime = rec[in_entry_time].split()[1].split("+")[0]
-      EndTime = rec[in_end_time].split()[1].split("+")[0]
-      Side = rec[in_side][:1]
+      StartTime = rec[in_entry_time].split()[1].split("+")[0] if len(rec[in_entry_time]) > 0 else ""
+      EndTime = rec[in_end_time].split()[1].split("+")[0] if len(rec[in_end_time]) > 0 else ""
+      Side = rec[in_side][:1] if len(rec[in_side]) > 0 else ""
       OrdQty = rec[in_order_quantity]
       LimitPx = rec[in_limit_price]
       MPA = rec[in_max_wpa]
@@ -77,13 +92,18 @@ def TestROD(args):
           OrdID, Symbol, Date, StartTime, EndTime, Side, OrdQty, LimitPx, MPA, ExecQty, ExecTime)
       sock.send(out_rec.encode("utf-8"))
     line = file.readline()
+    line_no += 1
 
   sockFile = sock.makefile()
-  while True:
+  line = sockFile.readline()
+  res = json.loads(line)
+  output_cnt = int(res["output_records"])
+  while line and output_cnt  > 0:
     line = sockFile.readline()
-    if len(line) == 0:
-      break
-    print(line[:-1])
+    output_cnt -= 1
+    #print(line[:-1])
+  line = sockFile.readline()
+  return json.loads(line)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
@@ -92,9 +112,19 @@ if __name__ == "__main__":
   parser.add_argument("-s", "--size", type=int, default=0, help="number of records in input file")
   try:
     args = parser.parse_args()
-    TestROD(args)
+    started = datetime.now()
+    runtime_summary = TestROD(args)
+    finished = datetime.now()
+    if runtime_summary:
+      request_parsing_sorting = StringToDuration(runtime_summary["request_parsing_sorting"])
+      execution = StringToDuration(runtime_summary["execution"])
+      result_merging_sorting = StringToDuration(runtime_summary["result_merging_sorting"])
+      print("started={} finished={} run-time={} request_parsing_sorting={} execution={} result_merging_sorting={}".format(
+        started, finished, finished - started, request_parsing_sorting, execution, result_merging_sorting))
+    else:
+      print("started:{} finished:{} query-time:{}".format(started, finished, finished - started))
   except IOError as e:
     if e.errno != 32: # broken pipe
       print("I/O error({0}): {1}".format(e.errno, e.strerror))
   except Exception as ex:
-    print("err:{}".format(ex))
+    print("err:{} line_no:{}".format(ex, line_no))
